8af7fe1a3b7a86a7a663adc55344d99303b63d1c
อภิศักดิ์ เกียรติขจรบำรุง <s6002041520182@Macmini-213-27.local>
2019-03-14 19:06:07 +0700

    exe1235

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-/**
- * @format
- */
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.js';
-import App from './components/App';
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
+import App from './components/App.js';
 
 AppRegistry.registerComponent(appName, () => App);

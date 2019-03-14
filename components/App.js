7c9025e4d2266dbb6f283b83c1033e4e3819e451
อภิศักดิ์ เกียรติขจรบำรุง <s6002041520182@Macmini-213-27.local>
2019-03-14 16:37:30 +0700

    Create App Components

---
+++ components/App.js
@@ -0,0 +1,17 @@
+//Import libary
+import React from 'react';
+import { View, Text } from 'react-native';
+
+//Write component
+class App extends React.Compenent {
+    render() {
+        return(
+            <View>
+                <Text>HELLO WORLD !!!</Text>
+            </View>
+            //Component
+        );
+    }
+}
+//Export
+export default App;

337fcd184dcbea38ca435e93f8a51f7feb09bbc4
อภิศักดิ์ เกียรติขจรบำรุง <s6002041520182@Macmini-213-27.local>
2019-03-28 14:21:30 +0700

    Create Header Component

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+// import library
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+// create component
+class Header extends Component {
+    render() {
+        return (
+            <View>
+                <Text>{this.props.title}</Text>
+            </View>        
+        );
+    }
+}
+// export
+export default Header;
